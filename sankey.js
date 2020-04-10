4573feaba0587fdb1a06b9daf4133789e3173a19
katiRG <cnangini@gmail.com>
2020-04-10 17:06:02 -0400

    WIP contd: rewrote BU json files with 'not affected' category

--- sankey.js
+++ sankey.js
@@ -84,7 +84,6 @@ d3.sankey = function() {
       node.targetLinks = [];
     });
     links.forEach(function(link) {
-      console.log("link: ", link)
       var source = link.source,
           target = link.target;
       if (typeof source === "number") source = link.source = nodes[link.source];
